1c30b65085e0e6db43411cc516a308950e1a33aa
swynd <scott.wynd@gmail.com>
2021-02-21 00:00:51 -0600

    bringing old fetch_tdem.py file back

---
+++ cloud_functions/fetch_tdem.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from datetime import datetime
+
+def _save_partitions(df, source='tdem'):
+    today = datetime.now().strftime('%Y-%m-%d')
+    hr_min = datetime.now().strftime('%H:%M')
+    df.to_csv(f's3://data-{source}/raw/{today}/{hr_min}/{source}-vaccine-supply.csv', index=False)
+    df.to_csv(f's3://data-{source}/raw/latest/{source}-vaccine-supply.csv', index=False)
+
+def main(event, context):
+    df = pd.read_csv("https://genesis.soc.texas.gov/files/accessibility/vaccineprovideraccessibilitydata.csv")
+    _save_partitions(df)
+    msg = f"{len(df)} records moved to S3 at {datetime.now().strftime('%Y-%m-%d')}"
+    print(msg)
+    response = {
+        "statusCode": 200,
+        "body": msg
+    }
+    return response
+
+# main("","")
