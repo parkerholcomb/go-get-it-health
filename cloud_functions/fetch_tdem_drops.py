6a9f256e35a8f492ba45b3a92514b65ba473fc4e
swynd <scott.wynd@gmail.com>
2021-02-22 23:46:38 -0600

    initial local testing for fetch_tdem_drops

--- cloud_functions/fetch_tdem_drops.py
+++ cloud_functions/fetch_tdem_drops.py
@@ -1,16 +1,19 @@
 import pandas as pd
-from datetime import datetime
+from datetime import datetime\
+# delete once testing is complete
+from datetime import timedelta
 import regex as re
 
 
 def _calc_drops(today, hr_min, new_df, old_df):
     dttm = datetime.strptime(f'{today} {hr_min}', '%Y-%m-%d %H:%M')
-    # lower case without special char > NAME#ZIP
     new_df['name_id'] = [f'{re.sub("[^0-9a-zA-Z]+", "", nm).lower()}#{zp}'
                          for nm, zp
                          in zip(new_df['NAME'], new_df['ZIP'])]
     new_df['extract_dttm'] = dttm
-    new_df['last_update_dttm'] = [datetime.strptime(f'{dt} {tm}', '%Y-%m-%d %H:%M')
+    new_df['LAST_UPDATE_VAC'] = new_df['LAST_UPDATE_VAC'].fillna(value='01/01/2021')
+    new_df['LAST_UPDATE_TIME_VAC'] = new_df['LAST_UPDATE_TIME_VAC'].fillna(value='00:00')
+    new_df['last_update_dttm'] = [datetime.strptime(f'{dt.strip()} {tm.strip()[:5]}', '%m/%d/%Y %H:%M')
                                   for dt, tm in zip(new_df['LAST_UPDATE_VAC'],
                                                     new_df['LAST_UPDATE_TIME_VAC'])]
     old_df['name_id'] = [f'{re.sub("[^0-9a-zA-Z]+", "", nm).lower()}#{zp}'
@@ -24,16 +27,16 @@ def _calc_drops(today, hr_min, new_df, old_df):
                            'VACCINES_AVAILABLE': 'old_available'},
                   inplace=True)
     new_counts = new_df[['name_id', 'new_shipped', 'new_available',
-                         'location_type', 'last_update_dttm']]
+                         'location_type', 'extract_dttm', 'last_update_dttm']]
     old_counts = old_df[['name_id', 'old_shipped', 'old_available']]
 
-    comp_df = new_counts.join(old_counts, on='name_id', how='left')
+    comp_df = new_counts.merge(old_counts, on='name_id', how='left')
     new_sites = comp_df[comp_df['old_shipped'].isnull()].reset_index(drop=True)
-    new_sites.rename(columns={'new_shipped': 'drop_ct'})
     new_sites['jab_ct'] = [0 if avail == 0 else shp - avail
                            for shp, avail
                            in zip(new_sites['new_shipped'],
                                   new_sites['new_available'])]
+    new_sites.rename(columns={'new_shipped': 'drop_ct'}, inplace=True)
 
     updates = comp_df[comp_df['old_shipped'].notnull()]
     updates['drop_ct'] = [new - old
@@ -46,7 +49,7 @@ def _calc_drops(today, hr_min, new_df, old_df):
                                 updates['new_available'],
                                 updates['drop_ct'])]
 
-    changes = updates[updates['drop_ct'] > 0 | updates['jab_ct'] > 0].reset_index(drop=True)
+    changes = updates[(updates['drop_ct'] > 0) | (updates['jab_ct'] > 0)].reset_index(drop=True)
 
     drop_df = pd.concat([new_sites[['name_id', 'location_type', 'extract_dttm', 'last_update_dttm',
                                     'drop_ct', 'jab_ct']],
@@ -65,16 +68,14 @@ def _save_partitions(today, hr_min, df, drop_df, source='tdem'):
 
 def main(event, context):
     today = datetime.now().strftime('%Y-%m-%d')
-    hr_min = datetime.now().strftime('%H:%M')
+    # hr_min = datetime.now().strftime('%H:%M')
+    # delete once testing is complete
+    tm_plus_six = datetime.now() + timedelta(hours=6)
+    hr_min = tm_plus_six.strftime('%H:%M')
 
     df = pd.read_csv("https://genesis.soc.texas.gov/files/accessibility/vaccineprovideraccessibilitydata.csv")
 
-    s3c = boto3.client("s3")
-    latest_obj = s3c.get_object(Bucket='data-tdem',
-                                Key='raw/latest/tdem-vaccine-supply.csv')
-    latest_body = latest_obj['Body']
-    latest_string = latest_body.read().decode('utf-8')
-    latest_df = pd.read_csv(StringIO(latest_string), sep='\t')
+    latest_df = pd.read_csv('s3://data-tdem/raw/latest/tdem-vaccine-supply.csv')
     drop_df = _calc_drops(today, hr_min, df, latest_df)
 
     _save_partitions(today, hr_min, df, drop_df)
@@ -86,4 +87,4 @@ def main(event, context):
     }
     return response
 
-# main("","")
+main("","")
