2b9ef79d8734482e628c2126f5f8f9aed88fccbb
Parker Holcomb <parker.holcomb@gmail.com>
2021-03-19 16:35:21 -0500

    push heb notifications with env/debug support

--- cloud_functions/push_heb_notification.py
+++ cloud_functions/push_heb_notification.py
@@ -18,8 +18,6 @@ def _get_current_prev_dfs():
     return [prev_df, current_df]
 
 def _get_filtered_location_updates(zip_, max_distance = 100):
-    zip_lat_lng = _geocode_zip(zip_)
-    
     prev_df,current_df = _get_current_prev_dfs()
     current_df = current_df[current_df['openAppointmentSlots'] > 0]
     prev_df[prev_df['name'].isin(current_df['name'])]
@@ -30,12 +28,16 @@ def _get_filtered_location_updates(zip_, max_distance = 100):
     # # and have one be the the same as the last
     # prev_df['openAppointmentSlots'][5] = 100
     
+    # generate updates...i.e. did new - old increase?
     updates = []
     for idx in current_df.index:
         if current_df['openAppointmentSlots'][idx] > prev_df['openAppointmentSlots'][idx]:
             updates.append(idx)
     df = current_df[current_df.index.isin(updates)]
+    
+    # then let's geocode it 
     df['lat_lng'] = df.apply(_geocode, axis=1)
+    zip_lat_lng = _geocode_zip(zip_)
     df['miles_away'] = df['lat_lng'].apply(lambda location_lat_lng: int(geodesic(location_lat_lng, zip_lat_lng).miles))
     df = df[df['miles_away'] < max_distance].sort_values(by='miles_away')
     print(df)
@@ -44,6 +46,10 @@ def _get_filtered_location_updates(zip_, max_distance = 100):
 def _geocode_zip(zip_ = "78741"):
     if zip_ == '78741':
         return '30.231252,-97.716'
+    elif zip_ == '75001':
+        return '32.95087730059655,-96.84214582595285'
+    elif zip_ == '78613':
+        return '30.5097186110076,-97.80432284502643'
     else:
         geolocator = Nominatim(user_agent="vtx")
         location = geolocator.geocode(zip_)
@@ -52,26 +58,49 @@ def _geocode_zip(zip_ = "78741"):
 def _geocode(row):
     return f"{row['latitude']},{row['longitude']}"
     
-def _generate_body(zip_ = "78741"):
-    df = _get_filtered_location_updates(zip_)
+def _generate_body(df):
     body = ['New availability detected at HEB:\n']
     for idx in df.index:
         body.append(f"💉 {df.loc[idx]['name']} has {df.loc[idx]['openAppointmentSlots']} appoinments, {df.loc[idx]['miles_away']} miles away")
     body.append('\nVisit https://vaccine.heb.com/scheduler to schedule. #goandgetgetit')
     return '\n'.join(body)
 
-def send_msg(zip_, to_):
-    body = _generate_body(zip_)
-    from_ = "+1 512 488 6383"
-    msg = client.messages.create(body=body,from_=from_,to=to_)
-    resp = f"Message sent {from_} to {to_} with locations -- {msg.sid}"
-    print(resp)
-    return resp
+def send_msg(zip_, to_, debug = False):
+    locations_df = _get_filtered_location_updates(zip_)
+    if len(locations_df) > 0:
+        body = _generate_body(locations_df)
+        from_ = "+1 512 488 6383"
+        if debug == False: 
+            msg = client.messages.create(body=body,from_=from_,to=to_)
+            resp = f"Message sent {from_} to {to_} with locations -- {msg.sid}"
+            print(resp)
+        else:
+            print(body)
+            resp = f"Message sent {from_} to {to_} with locations -- DEBUG=TRUE"
+        return resp
+
+    else: 
+        print(f'no updates within 100 miles for {zip_}')
 
 def main(event, context): 
-    send_msg('78741', "+1 713 824 8581") # parker
-    send_msg('78741', "‭+1 585 329 4284‬") # steve denero
-    send_msg('78741', "‭‭+1 817 637 2734‬") # scott wynd
+    # debug = True
+    debug = False
+    if debug: 
+        print("DEBUG MODE - WILL NOT SEND SMS")
+        to_df = pd.read_csv("./to_list-dev.csv")
+    else:
+        to_df = pd.read_csv("./to_list-prod.csv")
+    
+    print(to_df)
+    for i in range(len(to_df)):
+        obj = to_df.loc[i]
+        send_msg(obj['zip_'], obj['to_'], debug)
+        # send_msg('75001', "+1 713 824 8581") # parker
+        # send_msg('78741', "+1 713 824 8581") # parker
+        # send_msg('78741', "‭+1 585 329 4284‬") # steve denero
+        # send_msg('78741', "‭‭+1 817 637 2734‬") # scott wynd
+        # send_msg('78741', "‭‭+1 ‭650 740 9437‬‬") # daniel w
+        # send_msg('75001', "‭‭‭+1 214 490 5329‬‬‬") # dad
     
     response = {
         "statusCode": 200,
