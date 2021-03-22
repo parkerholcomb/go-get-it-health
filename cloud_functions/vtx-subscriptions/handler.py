c85940b1fbf7d1c671c7d274c2d6589c9afde8b2
Parker Holcomb <parker.holcomb@gmail.com>
2021-03-22 08:57:21 -0500

    inbound handler init

---
+++ cloud_functions/vtx-subscriptions/handler.py
@@ -0,0 +1,17 @@
+import json
+
+def main(event, context):
+    print("event:", event)
+    print("context:", context)
+    
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
