6cc1a336d9de9648cd2175b7140963d5208c9464
Parker Holcomb <parker.holcomb@gmail.com>
2021-04-04 10:30:30 -0500

    search route

---
+++ cloud_functions/search_handler.py
@@ -0,0 +1,68 @@
+import json
+import pandas as pd
+from lib.GeoZipCache import GeoZipCache
+from geopy.distance import geodesic
+
+geo_zip_cache = GeoZipCache()
+
+def _miles_away(lat_lng_a, lat_lng_b):
+    try:
+        return int(geodesic(lat_lng_a, lat_lng_b).miles)
+    except:
+        return 9999
+
+def _vaccine_type_stats(df):
+    return dict(
+        locations_count = int(df['NAME'].count()),
+        total_available = int(df['VACCINES_AVAILABLE'].sum()),
+        pfizer_available = int(df['PFIZER_AVAILABLE'].sum()),
+        moderna_available = int(df['MODERNA_AVAILABLE'].sum()),
+        jj_available = int(df['JJ_AVAILABLE'].sum())
+    )
+
+
+def _location_type_stats(df):
+    stats = df.groupby(['TYPE']).agg(
+        count=('NAME','count'), 
+        total_available=('VACCINES_AVAILABLE','sum'),
+        pfizer_available=('PFIZER_AVAILABLE','sum'),
+        moderna_available=('MODERNA_AVAILABLE','sum'),
+        jj_available=('JJ_AVAILABLE','sum')
+    ).reset_index()
+
+    return stats.to_dict(orient='records')
+
+def main(event, context):
+    body = eval(event['body'])
+    zip_lat_lng = geo_zip_cache.geocode_zip(body['zip_'])
+    df = pd.read_csv('https://genesis.soc.texas.gov/files/accessibility/vaccineprovideraccessibilitydata.csv')
+    df = df[df['VACCINES_AVAILABLE'] > 0]
+    print(f"There are {len(df)} locations in TX with availability")
+    df['lat_lng'] = df['ZIP'].apply(geo_zip_cache.geocode_zip)
+    df['miles_away'] = df['lat_lng'].apply(_miles_away, args=(zip_lat_lng,))
+    df = df[df['miles_away'] < body['radius']]
+    df = df.sort_values(by='miles_away').reset_index(drop=True)
+    
+    data = {
+        "stats": {
+            "byLocationType": _location_type_stats(df),
+            "byVaccineType": _vaccine_type_stats(df)
+        },
+        "locations": df.to_dict(orient='records')
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(data)
+    }
+
+    print(response)
+    return response
+
+if __name__ == "__main__":
+    body = {
+        'zip_': '78741',
+        'radius': 20
+    }
+    event = {'body': json.dumps(body)}
+    main(event,"")
