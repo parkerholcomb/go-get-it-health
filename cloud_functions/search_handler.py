fb25096c36c82fe0f6292da326c89fb19295e323
Parker Holcomb <parker.holcomb@gmail.com>
2021-04-05 07:41:43 -0500

    search_by_zip => http vs httpApi; df.head()

--- cloud_functions/search_handler.py
+++ cloud_functions/search_handler.py
@@ -23,7 +23,7 @@ def _vaccine_type_stats(df):
 
 def _location_type_stats(df):
     stats = df.groupby(['TYPE']).agg(
-        count=('NAME','count'), 
+        locations_count=('NAME','count'), 
         total_available=('VACCINES_AVAILABLE','sum'),
         pfizer_available=('PFIZER_AVAILABLE','sum'),
         moderna_available=('MODERNA_AVAILABLE','sum'),
@@ -33,27 +33,39 @@ def _location_type_stats(df):
     return stats.to_dict(orient='records')
 
 def main(event, context):
-    body = eval(event['body'])
-    zip_lat_lng = geo_zip_cache.geocode_zip(body['zip_'])
-    df = pd.read_csv('https://genesis.soc.texas.gov/files/accessibility/vaccineprovideraccessibilitydata.csv')
+    params = eval(event['body'])
+    zip_lat_lng = geo_zip_cache.geocode_zip(params['zip_'])
+    df = pd.read_csv('https://genesis.soc.texas.gov/files/accessibility/vaccineprovideraccessibilitydata.csv') # see if s3 is faster?
     df = df[df['VACCINES_AVAILABLE'] > 0]
     print(f"There are {len(df)} locations in TX with availability")
-    df['lat_lng'] = df['ZIP'].apply(geo_zip_cache.geocode_zip)
-    df['miles_away'] = df['lat_lng'].apply(_miles_away, args=(zip_lat_lng,))
-    df = df[df['miles_away'] < body['radius']]
-    df = df.sort_values(by='miles_away').reset_index(drop=True)
+    # df['lat_lng'] = df['ZIP'].apply(geo_zip_cache.geocode_zip)
+    # df['miles_away'] = df['lat_lng'].apply(_miles_away, args=(zip_lat_lng,))
+    # df = df[df['miles_away'] < params['radius']]
+    # df = df.sort_values(by='miles_away').reset_index(drop=True)
     
+    # data = {
+    #     "stats": {
+    #         "byLocationType": _location_type_stats(df),
+    #         "byVaccineType": _vaccine_type_stats(df)
+    #     },
+    #     "locations": df.to_dict(orient='records')
+    # }
+
+    df = df.head()
+
     data = {
-        "stats": {
-            "byLocationType": _location_type_stats(df),
-            "byVaccineType": _vaccine_type_stats(df)
-        },
-        "locations": df.to_dict(orient='records')
+        "byLocationType": _location_type_stats(df),
+        "byVaccineType": _vaccine_type_stats(df)
+        
     }
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(data)
+        "headers": {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': True,
+        },
+        "body": df.to_json(orient ='records')
     }
 
     print(response)
