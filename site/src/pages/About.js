1c7835273dcc829b6797c75d65cc00f97ebfe904
Parker Holcomb <parker.holcomb@gmail.com>
2021-04-10 12:36:44 -0500

    about

--- site/src/pages/About.js
+++ site/src/pages/About.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import {withRouter} from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import Nav from '../fragments/Nav'
 import PromptForm from '../fragments/PromptForm'
+import vaccinateImage from '../images/vaccinate.svg'
 
 class About extends Component {
     render() {
         return (
             <div className='container'>
-                <Nav/>
-                <div className='logoContainer'>
-                    todo
+                <Nav />
+                <div className="hero">
+                    <img src={vaccinateImage} />
                 </div>
-                <PromptForm/>
+
+                <div className="tagline">
+                    <span>Made with love by 
+                        <a href="https://www.linkedin.com/in/parkerholcomb/" target="_blank"> Parker</a>,
+                        <a href="https://www.linkedin.com/in/dwamian/" target="_blank"> Dwamian</a>,
+                        <a href="https://www.linkedin.com/in/scott-woolley-wynd-0299072a/" target="_blank"> Scott</a>. 
+                    </span>
+                </div>
+                {/* <PromptForm /> */}
             </div>
         )
     }
